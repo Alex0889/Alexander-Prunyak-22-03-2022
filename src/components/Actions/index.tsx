af5a06711623899238bbc74e2723d2f2f873a983
Alexander Prunyak <987456987p@gmail.com>
2022-04-01 22:16:20 +0300

    fixed bugs, added autocomplete component

--- src/components/Actions/index.tsx
+++ src/components/Actions/index.tsx
@@ -1,44 +1,31 @@
-import React, { ChangeEvent, FC, FormEvent, useEffect, useState } from 'react';
+import React, { FC, useEffect } from 'react';
 import s from './Actions.module.scss';
 import clsx from 'clsx';
-import _debounce from 'lodash.debounce';
 import GlobalSvgSelector from 'prebuild/assets/icons/GlobalSvgSelector';
-import Input from 'prebuild/components/Input';
 import { changeCssRootVariables } from 'prebuild/helpers/changeCssRootVariables';
 import { changeTheme } from 'app/slices/theme';
 import { Theme } from 'app/enum/Theme';
 import { changeLanguage } from 'app/slices/language';
 import { Lang } from 'app/enum/Lang';
-import { changeCity, changeKey } from 'app/slices/geolocation';
-import { transliterate } from 'prebuild/helpers/translitCyrillic';
 import { useAppDispatch, useAppSelector } from 'app/hooks';
-import withTranslate from '../WithTranslate';
-import { IWithTranslate } from 'prebuild/interfaces/IWithTranslate';
-import { Link, useLocation, useNavigate } from 'react-router-dom';
-import { storage } from 'prebuild/helpers/storage';
-import { getAutocomplete } from 'app/slices/autocomplete/thunk/getAutocomplete';
-import { clearAutocomplete } from 'app/slices/autocomplete';
+import { Link, useLocation } from 'react-router-dom';
+import Autocomplete from '../Autocomplete';
 
 type ActionsProps = {
   readonly className?: string;
 };
 
-const Actions: FC<ActionsProps & IWithTranslate> = (
+const Actions: FC<ActionsProps> = (
   {
     className,
-    t,
   }) => {
   const {
     theme: { theme },
     language: { lang },
-    autocomplete: { autocomplete: { autocomplete } },
   } = useAppSelector();
   const dispatch = useAppDispatch();
 
   const location = useLocation();
-  const navigate = useNavigate();
-
-  const [city, setCity] = useState<string>('');
 
   useEffect(() => {
     changeCssRootVariables(theme);
@@ -50,33 +37,6 @@ const Actions: FC<ActionsProps & IWithTranslate> = (
 
   const handleChangeLanguage = (): void => {
     dispatch(changeLanguage(lang === Lang.RU ? Lang.EN : Lang.RU));
-  };
-
-  const handleAutocomplete = (city: string, key: string): void => {
-    storage.setItem('city', city);
-    storage.setItem('cityKey', key);
-
-    dispatch(changeCity(city));
-    dispatch(changeKey(key));
-    dispatch(clearAutocomplete());
-
-    navigate('/');
-    setCity('');
-  };
-
-  const delayedQuery = _debounce(city => dispatch(getAutocomplete(city)), 1000);
-
-  const handleInput = (e: ChangeEvent<HTMLInputElement>) => {
-    setCity(e.target.value);
-    delayedQuery(e.target.value);
-  };
-
-  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-
-    dispatch(changeCity(transliterate(city)));
-    setCity('');
-    dispatch(clearAutocomplete());
   };
 
   return (
@@ -100,28 +60,10 @@ const Actions: FC<ActionsProps & IWithTranslate> = (
         <GlobalSvgSelector id='change-theme' />
       </button>
 
-      <form onSubmit={handleSubmit}>
-        <Input
-          value={city}
-          placeholder={t['searchCity']}
-          onChange={handleInput}
-        />
-        {autocomplete && autocomplete.length !== 0 &&
-        <div className={s.root__autocomplete}>{
-          autocomplete.map(city => (
-            <div key={city.Key} onClick={() => {
-              handleAutocomplete(city.LocalizedName, city.Key);
-            }}
-                 className={s.root__autocomplete_item}
-            >
-              {city.LocalizedName}
-            </div>))}
-        </div>
-        }
-      </form>
+      <Autocomplete />
 
     </div>
   );
 };
 
-export default withTranslate(Actions);
+export default Actions;
---
+++ src/components/Autocomplete/index.tsx
@@ -0,0 +1,97 @@
+import React, { ChangeEvent, FC, FormEvent, useEffect, useRef, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import s from './Autocomplete.module.scss';
+import _debounce from 'lodash.debounce';
+import { storage } from 'prebuild/helpers/storage';
+import { useAppDispatch, useAppSelector } from 'app/hooks';
+import { IWithTranslate } from 'prebuild/interfaces/IWithTranslate';
+import withTranslate from '../WithTranslate';
+import Input from 'prebuild/components/Input';
+import { getAutocomplete } from 'app/slices/autocomplete/thunk/getAutocomplete';
+import { changeCity, changeKey } from 'app/slices/geolocation';
+import { transliterate } from 'prebuild/helpers/translitCyrillic';
+import { clearAutocomplete } from 'app/slices/autocomplete';
+import clsx from 'clsx';
+
+type AutocompleteProps = {
+  readonly className?: string;
+};
+
+const Autocomplete: FC<AutocompleteProps & IWithTranslate> = (
+  {
+    className,
+    t,
+  }) => {
+  const { autocomplete: { autocomplete: { autocomplete } } } = useAppSelector();
+  const dispatch = useAppDispatch();
+
+  const navigate = useNavigate();
+  const ref = useRef<HTMLDivElement>(null);
+
+  const [city, setCity] = useState<string>('');
+
+  useEffect(() => {
+    function handleClickOutsideAutocomplete(event: MouseEvent) {
+      if (ref.current && !ref.current.contains(event.target as Node)) {
+        dispatch(clearAutocomplete());
+        setCity('');
+      }
+    }
+
+    document.addEventListener('click', handleClickOutsideAutocomplete);
+    return () => {
+      document.removeEventListener('click', handleClickOutsideAutocomplete);
+    };
+  }, [dispatch, ref]);
+
+  const handleAutocomplete = (city: string, key: string): void => {
+    storage.setItem('city', city);
+    storage.setItem('cityKey', key);
+
+    dispatch(changeCity(city));
+    dispatch(changeKey(key));
+    dispatch(clearAutocomplete());
+
+    navigate('/');
+    setCity('');
+  };
+
+  const delayedQuery = _debounce(city => dispatch(getAutocomplete(city)), 1000);
+
+  const handleInput = (e: ChangeEvent<HTMLInputElement>) => {
+    setCity(e.target.value);
+    delayedQuery(e.target.value);
+  };
+
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    dispatch(changeCity(transliterate(city)));
+    setCity('');
+    dispatch(clearAutocomplete());
+  };
+
+  return (
+    <form onSubmit={handleSubmit} className={clsx(s.root, className)}>
+      <Input
+        value={city}
+        placeholder={t['searchCity']}
+        onChange={handleInput}
+      />
+      {autocomplete && autocomplete.length !== 0 &&
+      <div ref={ref} className={s.root__cities}>{
+        autocomplete.map(city => (
+          <div key={city.Key} onClick={() => {
+            handleAutocomplete(city.LocalizedName, city.Key);
+          }}
+               className={s.root__cities_item}
+          >
+            {city.LocalizedName}
+          </div>))}
+      </div>
+      }
+    </form>
+  );
+};
+
+export default withTranslate(Autocomplete);
